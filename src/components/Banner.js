06b61c00ec8dea75379db1d94992cd057ecff5f4
Abdu9991 <abduadam688@gmail.com>
2023-08-13 00:12:31 -0400

    complete the assignment

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+import styling from "../components/styling.css";
+function Banner(){
+   return( <div className="flex-contianer">
+    <h1>Orbit Report</h1>
+    <p>Click on the button to see the satelllites in that orbit type</p>
+    </div>);
+}
+export default Banner;
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,24 @@ const Table = ({ sat }) => {
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <ht>Name</ht>
+          <th>Type of satellite</th>
+          <ht>Launch Date</ht>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((data, id) => {
+            return(
+               <tr>
+               <td>{data.name}</td>
+               <td>{data.type}</td>
+               <td>{data.launchDate}</td>
+               <td>{data.operational ? "Active" : "Inactive"}</td>
+              </tr>
+            )
+          })}
+        
         </tbody>
       </table>
   );
